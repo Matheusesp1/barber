84617d20a53573eec1fa80ecc7321bfbfafa9e47
Matheus <matheusespindola120@gmail.com>
2026-03-17 18:09:03 -0300

    feat: implement header component with logo and menu button style: update globals.css for improved theming and color variables chore: migrate tailwind configuration to TypeScript and enhance theme settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
-"use client"
-import { useState } from "react"
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+
 //SERVER COMPONENTS
 const Home = () => {
-  const [] = useState()
-  return <h1 className="text-red-500py">Hello world!</h1>
+  return (
+    <div>
+      {/* header */}
+      <Header />
+      <div className="p-5">
+        <h1 className="text-xl font-bold">Olá, Matheus</h1>
+        <p>Segunda-feira , 10 de abril de 2026</p>
+
+        <div className="flex items-center gap-2">
+          <input placeholder="Faça sua busca..."/>
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
 }
 export default Home
