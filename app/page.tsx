345f345ff961c939d9ddd9f2dcb1a66db14a782c
Matheus <matheusespindola120@gmail.com>
2026-03-19 17:50:56 -0300

    feat: add avatar and badge components, update styles for dark mode, and include banner image

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
-import { SearchIcon } from "lucide-react"
+import { Badge, SearchIcon } from "lucide-react"
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Avatar } from "@radix-ui/react-avatar"
+import { AvatarImage } from "@/components/ui/avatar"
+// import { Input } from "./_components/ui/input" 
 
-//SERVER COMPONENTS
+//SERVER
 const Home = () => {
   return (
     <div>
@@ -10,14 +15,39 @@ const Home = () => {
       <Header />
       <div className="p-5">
         <h1 className="text-xl font-bold">Olá, Matheus</h1>
-        <p>Segunda-feira , 10 de abril de 2026</p>
-
-        <div className="flex items-center gap-2">
-          <input placeholder="Faça sua busca..."/>
+        <p>Segunda-feira , 15 de abril de 2026</p>
+        {/* Busca */}
+        <div className="mt-6 flex items-center gap-2">
+          <input placeholder="Faça sua busca..." />
           <Button>
             <SearchIcon />
           </Button>
         </div>
+        {/* Imagem */}
+        <div className="relative mt-6 h-[155px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW"
+            src="/Banner.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+        {/* Agendamento */}
+        <Card className="mt-6">
+          <CardContent className="flex">
+            {/* Esquerda */}
+            <div className="relative h-[80px] w-[80px]">
+              <Badge>Confirmado</Badge>
+              <h3>Corte de Cabelo</h3>
+              
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="/Barbeiro.png" alt="Avatar do Barbeiro" />
+                </Avatar>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
